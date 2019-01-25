f7ffa10099f093f40a8166581b708c7b75359e09
Maud Laurent <tt2laurent.maud@gmail.com>
2019-01-25 17:12:55 +0100

    Fixed rendu 31 Janvier

--- src/main/ClientInterface.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ClientInterface extends Remote{
-	public void messageFromServer(String message) throws RemoteException;
-}
